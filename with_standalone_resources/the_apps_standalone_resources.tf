01487fac94f70649027438386195cec7c7ff1721
marianogg9 <gonzalez.mariano.gabriel@gmail.com>
2023-09-16 17:04:45 +0200

    added missing resources in standalone tf file

--- with_standalone_resources/the_apps_standalone_resources.tf
+++ with_standalone_resources/the_apps_standalone_resources.tf
@@ -37,6 +37,21 @@ resource "aws_s3_bucket_acl" "the_bucket_acl" {
   acl = "private"
 }
 
+resource "aws_s3_bucket_public_access_block" "the_bucket_ab" {
+  for_each = {
+    app-1 = { "permissions" : ["s3:PutObject"] },
+    app-2 = { "permissions" : ["s3:GetObject", "s3:PutObject"] },
+    app-3 = { "permissions" : ["s3:GetObjectVersion"] }
+  }
+
+  bucket = aws_s3_bucket.the_bucket[each.key].id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 # IAM resources
 
 ## Some prerequisites
@@ -89,4 +104,37 @@ resource "aws_iam_role" "the_role" { # create the IAM role and attach both assum
   tags = {
     # always add some tags!
   }
+}
+
+resource "aws_iam_policy" "the_policy" {
+  for_each = {
+    app-1 = { "permissions" : ["s3:PutObject"] },
+    app-2 = { "permissions" : ["s3:GetObject", "s3:PutObject"] },
+    app-3 = { "permissions" : ["s3:GetObjectVersion"] }
+  }
+
+  name_prefix = join("",[each.key,"-"])
+  path        = "/"
+
+  policy      = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Action   = [ for permission in each.value["permissions"]: permission ]
+        Effect   = "Allow"
+        Resource = join("",[aws_s3_bucket.the_bucket[each.key].arn,"/*"])
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "the_policy_attachment" {
+  for_each = {
+    app-1 = { "permissions" : ["s3:PutObject"] },
+    app-2 = { "permissions" : ["s3:GetObject", "s3:PutObject"] },
+    app-3 = { "permissions" : ["s3:GetObjectVersion"] }
+  }
+
+  role       = aws_iam_role.the_role[each.key].name
+  policy_arn = aws_iam_policy.the_policy[each.key].arn
 }
