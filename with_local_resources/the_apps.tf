6d844c4106f32bfe6180ac074e19c3451bfa8075
marianogg9 <gonzalez.mariano.gabriel@gmail.com>
2023-09-16 11:14:56 +0200

    added standalone resources creation + for each clauses

--- with_local_resources/the_apps.tf
+++ with_local_resources/the_apps.tf
@@ -1,10 +1,23 @@
 # S3 resources
 resource "aws_s3_bucket" "the_bucket" {
-  bucket_prefix = "app-1-"
+  for_each = {
+    app-1 = { "permissions" : ["s3:PutObject"] },
+    app-2 = { "permissions" : ["s3:GetObject", "s3:PutObject"] },
+    app-3 = { "permissions" : ["s3:GetObjectVersion"] }
+  }
+
+  bucket_prefix = join("", [each.key, "-"])
 }
 
 resource "aws_s3_bucket_ownership_controls" "the_bucket_oc" {
-  bucket = aws_s3_bucket.the_bucket.id
+  for_each = {
+    app-1 = { "permissions" : ["s3:PutObject"] },
+    app-2 = { "permissions" : ["s3:GetObject", "s3:PutObject"] },
+    app-3 = { "permissions" : ["s3:GetObjectVersion"] }
+  }
+
+  bucket = aws_s3_bucket.the_bucket[each.key].id
+  
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
@@ -13,47 +26,67 @@ resource "aws_s3_bucket_ownership_controls" "the_bucket_oc" {
 resource "aws_s3_bucket_acl" "the_bucket_acl" {
   depends_on = [aws_s3_bucket_ownership_controls.the_bucket_oc]
 
-  bucket     = aws_s3_bucket.the_bucket.id
-  acl        = "private"
+  for_each = {
+    app-1 = { "permissions" : ["s3:PutObject"] },
+    app-2 = { "permissions" : ["s3:GetObject", "s3:PutObject"] },
+    app-3 = { "permissions" : ["s3:GetObjectVersion"] }
+  }
+
+  bucket = aws_s3_bucket.the_bucket[each.key].id
+
+  acl = "private"
 }
 
 # IAM resources
 
 ## Some prerequisites
 data "aws_eks_cluster" "this" { # get EKS cluster attributes to use later on.
-    name = "your-eks-cluster-name"
+  name = "test-eks-cluster"
 }
 
+data "aws_caller_identity" "current" {} # get current account ID
+
 data "aws_iam_policy_document" "assume-policy" { # create an assume policy for STS
-    statement {
-      actions = ["sts:AssumeRoleWithWebIdentity"]
-      principals {
-        type        = "Federated"
-        identifiers = [data.aws_eks_cluster.this.identity[0].oidc[0].issuer]
-      }
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type = "Federated"
+      identifiers = [
+        replace(
+          data.aws_eks_cluster.this.identity[0].oidc[0].issuer,
+          "https://",
+          join("", ["arn:aws:iam::", data.aws_caller_identity.current.account_id, ":oidc-provider/"])
+        )
+      ]
     }
+  }
 }
 
 ## Role definition
 resource "aws_iam_role" "the_role" { # create the IAM role and attach both assume and inline identity based policies.
-    name_prefix        = "app-1-"
-    path               = "/"
-    assume_role_policy = data.aws_iam_policy_document.assume-policy.json
-    inline_policy {
-      name   = "s3-put"
-      policy = jsonencode({
-        Version   = "2012-10-17"
-        Statement = [{
-            Action = [
-                "s3:PutObject"
-            ]
-            Effect   = "Allow"
-            Resource = aws_s3_bucket.the_bucket.arn
-        }]
-      })
-    }
+  for_each = {
+    app-1 = { "permissions" : ["s3:PutObject"] },
+    app-2 = { "permissions" : ["s3:GetObject", "s3:PutObject"] },
+    app-3 = { "permissions" : ["s3:GetObjectVersion"] }
+  }
 
-    tags = {
-        # always add some tags!
-    }
+  name_prefix = join("", [each.key, "-"])
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.assume-policy.json
+
+  inline_policy {
+    name = "s3-put"
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [{
+        Action = [for permission in each.value["permissions"] : permission]
+        Effect = "Allow"
+        Resource = aws_s3_bucket.the_bucket[each.key].arn
+      }]
+    })
+  }
+
+  tags = {
+    # always add some tags!
+  }
 }
